9e08e688599d0bd288f656753ee265f50437ab5e
Conner Cowling <connercarl@gmail.com>
2022-01-21 10:43:21 -0600

    Basic react app with tailwind, tailwind typography working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Example from "./routes/AppShell"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <Example />;
 }
 
-export default App;
+// export default function App() {
+//   return (
+//     <Router>
+//       <div>
+//       <h1 className="text-3xl font-bold underline">BASIC APP</h1>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/about">About</Link>
+//             </li>
+//             <li>
+//               <Link to="/users">Users</Link>
+//             </li>
+//           </ul>
+//         </nav>
+
+//         {/* A <Routes> looks through its children <Route>s and
+//             renders the first one that matches the current URL. */}
+//         <Routes>
+//           <Route path="/about" element={<About />} />
+//           <Route path="/users" element={<Users />} />
+//           <Route path="/" element={<Example />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
